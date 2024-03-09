6992e0b9eaea274cdba1451bbabbb0f4b1403ad2
Mohammed Faizan <mohdfaizan.zprb2@gmail.com>
2024-03-09 09:55:50 +0530

    Initial setup with fork fixes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+
+const App = () => {
+  return (
+    <div>App
+    <Button variant="secondary">Hi</Button>
+    </div>
+  )
+}
+
+export default App
